50b2c89d049d559551e5e3e9ee12757c5ef22c07
Lunic Dumitru <90331529+ChefDimas@users.noreply.github.com>
2023-01-10 21:19:14 +0100

    Make text color to be a complementary

--- Day1-ChangeBackgroundColor/index.js
+++ Day1-ChangeBackgroundColor/index.js
@@ -1,10 +1,22 @@
+// return random color
 const randomColor = () => {
     return Math.floor(Math.random() * 16777215).toString(16);
 };
 
+// invert hexcolor, give complementary color
+function invertHex(hex) {
+    return (Number(`0x1${hex}`) ^ 0xffffff)
+        .toString(16)
+        .substr(1)
+        .toUpperCase();
+}
+
+//change colors
 const changeColor = () => {
     let color = randomColor();
+    let invertColor = invertHex(color);
     document.getElementById("page").style.backgroundColor = "#" + color;
     document.getElementById("color").innerHTML = "#" + color;
-    document.getElementById("button").style.color = "#" + randomColor();
+    document.getElementById("button").style.color = "#" + invertColor;
+    document.getElementById("color").style.color = "#" + invertColor;
 };
