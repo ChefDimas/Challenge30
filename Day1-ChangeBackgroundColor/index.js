b677075c2e3aa2734e4170cbdb20743a6af1cfc7
Lunic Dumitru <90331529+ChefDimas@users.noreply.github.com>
2023-01-10 21:50:50 +0100

    Change from HEX to HSL

--- Day1-ChangeBackgroundColor/index.js
+++ Day1-ChangeBackgroundColor/index.js
@@ -1,4 +1,4 @@
-// return random color
+// return random HEX color
 const randomColor = () => {
     return Math.floor(Math.random() * 16777215).toString(16);
 };
@@ -11,12 +11,21 @@ function invertHex(hex) {
         .toUpperCase();
 }
 
+// return random HSL Color
+const randomHSLColor = () => {
+    h = Math.floor(Math.random() * 360);
+    s = Math.floor(Math.random() * 101) + "%";
+    l = Math.floor(Math.random() * 101) + "%";
+    return `hsl(${h}, ${s}, ${l})`;
+};
+console.log(randomHSLColor());
+
 //change colors
 const changeColor = () => {
-    let color = randomColor();
+    let color = randomHSLColor();
     let invertColor = invertHex(color);
-    document.getElementById("page").style.backgroundColor = "#" + color;
-    document.getElementById("color").innerHTML = "#" + color;
-    document.getElementById("button").style.color = "#" + invertColor;
+    document.getElementById("page").style.backgroundColor = color;
+    document.getElementById("color").innerHTML = color;
     document.getElementById("color").style.color = "#" + invertColor;
+    document.getElementById("button").style.color = "#" + invertColor;
 };
