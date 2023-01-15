ca16e133a6daec60e666dd71b13d26f9cb451f46
Lunic Dumitru <90331529+ChefDimas@users.noreply.github.com>
2023-01-15 20:37:20 +0100

    Changes

--- Day4-DrumKit/index.js
+++ Day4-DrumKit/index.js
@@ -1,5 +1,7 @@
-document.querySelector("button").addEventListener("click", handleClick());
-
-const handleClick = () => {
-    alert("dsada");
-};
+for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
+    document.querySelector(".drum")[i].addEventListener("click", function () {
+        const audio = new Audio("./assets/sounds/tom-1.mp3");
+        audio.play();
+        console.log("dsa");
+    });
+}
