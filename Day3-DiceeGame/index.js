85bd0d62a5551765b85171d02e7d4062f6720e94
Lunic Dumitru <90331529+ChefDimas@users.noreply.github.com>
2023-01-15 15:35:03 +0100

    Project finished

--- Day3-DiceeGame/index.js
+++ Day3-DiceeGame/index.js
@@ -9,33 +9,32 @@ let player2 = null;
 const reset = () => {
     document.querySelector("img").classList.remove("image1");
     document.querySelector("img").classList.remove("image2");
-    document.querySelector(".player1").innerHTML += "";
-    document.querySelector(".player2").innerHTML += "";
+    document.querySelector(".player1").innerHTML = "";
+    document.querySelector(".player2").innerHTML = "";
 };
 
 const start = () => {
+    reset();
     let flag = false;
     player1 = randomNumber();
     player2 = randomNumber();
     if (flag === false) {
+        for (let n = 0; n < player1; n++) {
+            document.querySelector(".player1").innerHTML +=
+                '<div class="bullet"></div>';
+        }
+        for (let m = 0; m < player2; m++) {
+            document.querySelector(".player2").innerHTML +=
+                '<div class="bullet"></div>';
+        }
         if (player1 > player2) {
             document.querySelector("h1").textContent = "Player 1 Wins!";
-            // document.querySelector("img").classList.remove("image2");
             document.querySelector("img").classList.add("image1");
-            for (let n = 0; n < player1 + 1; n++) {
-                document.querySelector(".player1").innerHTML +=
-                    '<div class="bullet"></div>';
-                flag = true;
-            }
+            flag = true;
         } else if (player1 < player2) {
             document.querySelector("h1").textContent = "Player 2 Wins!";
-            // document.querySelector("img").classList.remove("image1");
             document.querySelector("img").classList.add("image2");
-            for (let n = 0; n < player2 + 1; n++) {
-                document.querySelector(".player2").innerHTML +=
-                    '<div class="bullet"></div>';
-                flag = true;
-            }
+            flag = true;
         } else {
             document.querySelector("h1").textContent = "It's a Draw";
             document.querySelector("img").classList.remove("image1");
