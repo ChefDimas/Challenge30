aa663df7d6d1d95395592a0220091dbfc58e7b52
Lunic Dumitru <90331529+ChefDimas@users.noreply.github.com>
2023-01-16 16:35:01 +0100

    Fixed

--- Day4-DrumKit/index.js
+++ Day4-DrumKit/index.js
@@ -1,7 +1,9 @@
 for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
-    document.querySelector(".drum")[i].addEventListener("click", function () {
-        const audio = new Audio("./assets/sounds/tom-1.mp3");
-        audio.play();
-        console.log("dsa");
-    });
+    document
+        .querySelectorAll(".drum")
+        [i].addEventListener("click", function () {
+            var audio = new Audio("./assets/sounds/tom-1.mp3");
+            audio.play();
+            console.log("dsa");
+        });
 }
