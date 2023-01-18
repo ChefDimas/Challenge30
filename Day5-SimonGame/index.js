dc592512786430d5fe5742af4a90c5f1e17355b9
Lunic Dumitru <90331529+ChefDimas@users.noreply.github.com>
2023-01-18 22:35:58 +0100

    Function to random extract a color from array

--- Day5-SimonGame/index.js
+++ Day5-SimonGame/index.js
@@ -1,10 +1,27 @@
 let level = 1;
+const green = $(".green");
+const red = $(".red");
+const yellow = $(".yellow");
+const blue = $(".blue");
+
+const colors = [green, red, yellow, blue];
+
+//Random pick a color
+const randomColor = () => {
+    return colors[Math.floor(Math.random() * colors.length)];
+};
 
 //Function to start the game
 const gameStart = () => {};
 
+//Generate Levels
+const generateLevels = () => {};
+
+//Main
 $("body").keypress(function () {
     console.log("Game started...");
     $("h1").html(`Level ${level}`);
     gameStart();
 });
+
+console.log(randomColor());
