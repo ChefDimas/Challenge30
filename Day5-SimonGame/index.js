075118e628eb923dcd2e0826d0e0ec83b002deff
Lunic Dumitru <90331529+ChefDimas@users.noreply.github.com>
2023-01-19 22:29:36 +0100

    Changes

--- Day5-SimonGame/index.js
+++ Day5-SimonGame/index.js
@@ -1,11 +1,17 @@
-const buttonColors = ["red", "blue", "green", "yellow"];
+const buttonColors = ["green", "red", "yellow", "blue"];
 const gamePattern = [];
 
 const nextSequence = () => {
-    return (randomNumber = Math.floor(Math.random() * 4));
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+
+    $("#" + randomChosenColor)
+        .fadeIn(100)
+        .fadeOut(100)
+        .fadeIn(100);
+
+    var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+    audio.play();
 };
-
-const randomChosenColor = buttonColors[nextSequence()];
-gamePattern.push(randomChosenColor);
-
 console.log(gamePattern);
