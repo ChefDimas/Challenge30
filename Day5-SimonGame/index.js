3bccc4c383f55a50b7ad209bdd672be3ebe8f885
Lunic Dumitru <90331529+ChefDimas@users.noreply.github.com>
2023-01-20 12:32:30 +0100

    Changes

--- Day5-SimonGame/index.js
+++ Day5-SimonGame/index.js
@@ -1,13 +1,22 @@
 const buttonColors = ["green", "red", "yellow", "blue"];
 const gamePattern = [];
+let level = 0;
+let start = false;
 
 var userClickedPattern = [];
 
-$(".btn").click(function () {
-    var userChosenColor = $(this).attr("id");
-
-    userClickedPattern.push(userChosenColor);
+$(document).keypress(function () {
+    if (!start) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
 });
+
+const playSound = (name) => {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+};
 
 const nextSequence = () => {
     var randomNumber = Math.floor(Math.random() * 4);
@@ -19,6 +28,11 @@ const nextSequence = () => {
         .fadeOut(100)
         .fadeIn(100);
 
-    var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
-    audio.play();
+    playSound(randomChosenColor);
 };
+
+$(".btn").click(function () {
+    var userChosenColor = $(this).attr("id");
+
+    userClickedPattern.push(userChosenColor);
+});
