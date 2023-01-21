96342af7b96ea1bb5e4dbe11f4f9c523432600bb
Lunic Dumitru <90331529+ChefDimas@users.noreply.github.com>
2023-01-21 19:24:30 +0100

    Changes

--- Day6-Calculator/server.js
+++ Day6-Calculator/server.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+    let num1 = Number(req.body.n1);
+    let num2 = Number(req.body.n2);
+
+    let result = num1 + num2;
+
+    res.send(`The result of calculation is ${result}`);
 });
 
 app.listen(port, () => {
